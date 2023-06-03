6ff17965dedb489b46f65236673a7f1626fe2bb0
krstn2140 <114401721+krstn2140@users.noreply.github.com>
2023-06-03 21:47:57 +0300

    Update Praktikum.java
    Print

--- Praktikum.java
+++ Praktikum.java
@@ -2,5 +2,6 @@ public class Praktikum{
         public static void main(String[] args) {
           String name = "Кристина";
           int age = 22;
+          System.out.println(name, age);
         }
 }
